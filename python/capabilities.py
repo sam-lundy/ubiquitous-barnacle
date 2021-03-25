941e5916af30699a8493f62e832fb48212675514
exion89 <samuel.lundy@gmail.com>
2021-03-25 13:49:31 -0600

    Updated get scripts

---
+++ python/capabilities.py
@@ -0,0 +1,22 @@
+from ncclient import manager
+import logging
+
+#logging.basicConfig(level=logging.DEBUG)
+
+router = {
+    "host": "ios-xe-mgmt-latest.cisco.com",
+    "port": "830",
+    "username": "developer",
+    "password": "C1sco12345"
+}
+
+with manager.connect(**router, hostkey_verify=False) as m:
+    for capability in m.server_capabilities:
+        print('*' * 25)
+        print(' ')
+        print(capability)
+
+
+
+
+
